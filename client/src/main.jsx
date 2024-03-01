a9ecb314c49832f637b965ad4e37be86c3a2ff96
Daninda <officialdannisimo@gmail.com>
2024-03-01 15:11:44 +0400

    ui fix

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <ToastContainer
         toastClassName={
-          'mb-4 ml-4 w-[calc(100%-2rem)] sm:w-auto sm:mb-auto rounded bg-surface'
+          'mb-4 ml-4 w-[calc(100%-2rem)] sm:mb-auto rounded bg-surface'
         }
         bodyClassName={'text-base text-textColor'}
         position='bottom-center'
